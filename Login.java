ecc108e2da1256c74428d51e3ae72078d1468877
Aidan-Gaskin <aidan-gaskin@hotmail.com>
2023-04-18 13:25:30 +0100

    Initial Draft of the login class - creating the password and access functionaltiy of accounts

---
+++ Login.java
@@ -0,0 +1,20 @@
+package GSM;
+
+
+/*
+ * This class will enable the creation of a password
+ * and allow users access to their account object 
+ * 
+ * 
+ * initial draft - nothing is finalised (not even the class)
+ * 
+ */
+
+public class Login 
+{
+	
+	
+	
+	
+
+}
